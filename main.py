7343594f7114d4cae2477027470ff912863f2288
may811204 <ju40268@gmail.com>
2017-07-11 12:05:34 +0900

    add how many user per country, city

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import itertools
 import csv
 import os
 import numpy as np
-# for the visualization
-# https://www.datawrapper.de/map/BBqiS/visualize
-# http://datamaps.github.io/
+
 #-------------------------------------------------------
 # for the geographic mapping using GeoDB
 import geoip2.database
@@ -51,15 +49,50 @@ def concurrent_max_user_online(split_data):
 # for concurrent max user per country
 # https://stackoverflow.com/questions/33706408/how-to-sort-by-value-efficiently-in-pyspark
 # https://stackoverflow.com/questions/30787635/takeordered-descending-pyspark
+# def concurrent_max_user_per_country(split_data):
+#     city_mapping = split_data.map(lambda data: data[4]).mapPartitions(partitionIp2city)
+#     city_count = city_mapping.map(lambda x:(x,1)).reduceByKey(add)
+#     sorted_city_count = city_count.sortBy(lambda x: -x[1]).collect()
+#     # for sorting in the descending order.
+#     for (city, count) in sorted_city_count:
+#         print("%s: %i" % (city, count))
+#     print('#'*40)
+#     return city_count.collect()
+    #return city_count
+
+# for concurrent max user per country
+# https://stackoverflow.com/questions/29717257/pyspark-groupbykey-returning-pyspark-resultiterable-resultiterable
 def concurrent_max_user_per_country(split_data):
-    city_mapping = split_data.map(lambda data: data[4]).mapPartitions(partitionIp2city)
-    city_count = city_mapping.map(lambda x:(x,1)).reduceByKey(add)
-    sorted_city_count = city_count.sortBy(lambda x: -x[1]).collect()
-    # for sorting in the descending order.
-    for (city, count) in sorted_city_count:
-        print("%s: %i" % (city, count))
-    print('#'*40)
-    return city_count.collect()
+    id_collect = split_data.map(lambda data: data[3]).collect()
+    ip_collect = split_data.map(lambda data: data[4]).mapPartitions(partitionIp2city).collect()
+    id_ip_pair = zip(ip_collect,id_collect)
+    pair = sc.parallelize(id_ip_pair)
+    # return the total amount of transaction according to a specific area.
+    print pair.groupByKey().mapValues(len).sortBy(lambda x: -x[1]).collect()
+    print '#'*40
+    distinct_count = pair.distinct().keys().map(lambda x: (x, 1)).reduceByKey(add)
+    print distinct_count.sortBy(lambda x: -x[1]).collect()
+    print '#'*40
+    #print sorted(x.groupByKey().map(lambda x : (x[0], list(x[1]))).collect())
+    #for country, id_list in id_ip
+    
+    #id_ip_rdd = sc.parallelize(id_ip_pair)
+    #print id_ip_rdd.take(10)
+    #for user_id, ip in sorted(id_ip_rdd.groupByKey().distinct()):
+    #    print user_id, ip.distinct()
+
+    #print sorted(temp.groupByKey().collect())
+
+    # print split_data.map(lambda data: (data[3], data[4]))
+    # print split_data.map(lambda data: data[3]).distinct().count()
+    # print city_mapping.take(300)
+    
+    
+def geo_active_user(split_data):
+    return split_data.map(lambda data: data[3]).distinct().collect()
+def geo_most_active_user(split_data):
+    return split_data.map(lambda data: data[3]).distinct().collect()
+
 
 def list_concat(param_list):
     return sc.parallelize(list(itertools.chain.from_iterable(param_list)))
@@ -68,13 +101,28 @@ def make_pair(list1, list2):
     return zip(list1, list2)
     
 if __name__ == "__main__":
-    Flag_max_user_per_country = False
-    Flag_max_user_online = True
+    Flag_geo_active_user = False
+    Flag_max_user_online = False
+    Flag_max_user_per_country = True
     
     f_list = traverse(data_dir)
-     
+    
+    geo_active_user_list = []
+    
     concurrent_max_user_online_list = []
     concurrent_max_user_per_country_list = []
+    
+    
+    # how many distinct users during all 2016-2017
+    if Flag_geo_active_user:
+        for f in f_list:
+            split_data = parser(f)
+            geo_active_user_list.append(geo_active_user(split_data))
+        flat_geo_active_user = list_concat(geo_active_user_list)
+        print flat_geo_active_user.distinct().count()
+    
+    
+    # for distinct user # on specific time period(1Hr)
     if Flag_max_user_online:
         for f in f_list:
             split_data = parser(f)
@@ -87,10 +135,10 @@ if __name__ == "__main__":
         
         
     if Flag_max_user_per_country:
-        for i in range(len(f_list)):
+        for i in range(2):
             split_data = parser(f_list[i])
             concurrent_max_user_per_country_list.append(concurrent_max_user_per_country(split_data))
-        flat_country = list_concat(concurrent_max_user_per_country_list)
-        print flat_country.reduceByKey(add).sortBy(lambda x: -x[1]).collect()
+        #flat_country = list_concat(concurrent_max_user_per_country_list)
+        #print flat_country.reduceByKey(add).sortBy(lambda x: -x[1]).collect()
     
 
